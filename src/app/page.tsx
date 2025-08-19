b8bf4af0ffb3959e6376d8984f84430039f3ae8a
smdekatepro <smdekatepro@gmail.com>
2025-08-19 19:23:36 +0530

    add neobrutalism inspired customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Progress } from "@/components/ui/progress";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-red-500 text-center mt-5 text-3xl italic">
-        Hello, World!
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>Clicable Button</Button>
+        </div>
+
+        <div>
+          <Input placeholder="i am an input" />
+        </div>
+
+        <div>
+          <Progress value={33} />
+        </div>
+
+        <div>
+          <Textarea value={"I am a text area"} />
+        </div>
+
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
